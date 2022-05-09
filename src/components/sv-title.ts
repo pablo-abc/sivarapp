9fc4bd8fd1e365397d46b0b812e75e54f56f9491
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-05-09 23:40:00 +0200

    feat: add drawer

--- src/components/sv-title.ts
+++ src/components/sv-title.ts
@@ -1,16 +1,48 @@
+import type { SlDrawer } from '@shoelace-style/shoelace';
 import { getInstance } from '@api/instance';
-import { LitElement, html, PropertyValues } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import type { PropertyValues } from 'lit';
+import { LitElement, html, css } from 'lit';
+import { customElement, property, query } from 'lit/decorators.js';
 
-export const tagName = 'sv-title';
+import '@shoelace-style/shoelace/dist/components/drawer/drawer.js';
+import '@shoelace-style/shoelace/dist/components/icon-button/icon-button.js';
+import '@components/sv-signout-button';
 
-@customElement(tagName)
+@customElement('sv-title')
 export class SvTitle extends LitElement {
+  static styles = css`
+    #header {
+      display: flex;
+      align-items: center;
+    }
+
+    sl-icon-button {
+      font-size: 2rem;
+    }
+
+    li {
+      list-style-type: none;
+      margin-top: 1rem;
+    }
+
+    a {
+      color: var(--sl-color-primary-800, blue);
+      text-decoration: none;
+    }
+
+    a:visited {
+      color: var(--sl-color-primary-800, blue);
+    }
+  `;
+
   @property()
   heading = '';
 
   @property()
   instance = localStorage.getItem('currentInstance') || 'sivar.cafe';
+
+  @query('sl-drawer')
+  drawer!: SlDrawer;
 
   connectedCallback() {
     super.connectedCallback();
@@ -30,7 +62,33 @@ export class SvTitle extends LitElement {
     }
   }
 
+  openDrawer() {
+    this.drawer.show();
+  }
+
   override render() {
-    return html`<h1>${this.heading}</h1>`;
+    return html`
+      <div id="header">
+        <sl-icon-button
+          @click=${this.openDrawer}
+          name="list"
+          label="Open menu"
+        ></sl-icon-button>
+        <h1>${this.heading}</h1>
+      </div>
+      <sl-drawer>
+        <ul>
+          <li>
+            <a href="/profile">Profile</a>
+          </li>
+          <li>
+            <a href="/preferences">Preferences</a>
+          </li>
+          <li>
+            <sv-signout-button></sv-signout-button>
+          </li>
+        </ul>
+      </sl-drawer>
+    `;
   }
 }
